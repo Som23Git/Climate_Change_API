383942aaa6dc90107cb5da401af3ec6600ffad2f
Som23Git <somesh.rokz@gmail.com>
2021-12-13 20:28:42 +0530

    Intermediate Code changes until base url

---
+++ index.js
@@ -0,0 +1,60 @@
+const PORT = 8080;
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const res = require('express/lib/response');
+
+const app = express();
+
+const newspapers = [
+    {
+        name:'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
+    },
+    {
+        name:'guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+        base: ''
+    },
+    {
+        name:'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+        base: 'https://www.telegraph.co.uk'
+    }
+]
+
+const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+      .then(response => {
+          const html = response.data
+          const $ = cheerio.load(html)
+
+          $('a:contains("climate")', html).each(function(){
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                }) 
+            })
+    })
+})
+
+app.get('/', (req,res) => {
+    res.json('Welcome to my Climate Change News API')
+});
+
+app.get('/news', (req,res) => {
+   res.json(articles);
+})
+
+app.get('/news/:newspaperId', async(req,res) => {
+    console.log(req)
+})
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
